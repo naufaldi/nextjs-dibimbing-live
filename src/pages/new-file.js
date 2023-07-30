3af9cf20dad92ee206cd99b55f6f11b27760abec
Naufaldi Rafif S <naufaldi.rafif@efishery.com>
2023-07-30 21:28:45 +0700

    new file add

---
+++ src/pages/new-file.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NewFile = () => {
+  return (
+    <div className="max-w-screen-sm mx-auto bg-slate-100">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel molestiae
+      sit, explicabo officiis maiores animi commodi accusantium consequuntur
+      laborum nihil modi velit cumque nobis neque non error, aspernatur cum?
+      Inventore.
+    </div>
+  );
+};
+
+export default NewFile;
